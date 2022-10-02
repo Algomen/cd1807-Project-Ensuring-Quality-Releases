18a3b65787efde9d4f59f427e0f2aef1e9b31d7c
Álvaro Gómez <alvarogomen93@gmail.com>
2022-10-02 17:42:24 +0200

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -19,7 +19,7 @@ resource "azurerm_linux_virtual_machine" "test" {
   admin_username      = var.vm_admin_username
   network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
-    username   = "alvaro"
+    username   = var.vm_admin_username
     public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDBsU/vDAulhsxQVn6BnUip0RY1xXgc0M3+icl47Wu31o9U+dSitPfCk9quqiLuFY6ouHPJanp04u3C3gWoldUeMG2Q66Y436gXAlD8s/jq5OY/cfQtaCFK+DUvEZqlOQO6fq8QieS2YU8tMIxiPHWf6vGwMJNxzjs2pOk1NaP0aGTPb0I22V5Tu+/m+qCCVoBFjPLDQhzPPmn+2zlhJWTMrz2oaEwFwmnRDaNPjqHqqrzOGOndx+k9a0680mW9Dx+Zvgc37Y1b9mNgbDYzztMXWs1+8HGB/QQxAULQ2/efixx4PtHr+39whOpVWnHpbxCxLFSS8lKrCYmLE8Qk0cR/+V8jtg414wME8mCUDEQN7NYMywG6iFkd48VCbf7kOncutHwG3UELe7KKCbCPMRsgZfGxo7t8fUFtjxcxtnrdTFVut7yBP4m2mvQlZ5P7IpKGnht+WQPURozn9bP4Trd56GpIBj3Y2BTxhRtLxT/MYulFzziWppSagC0ooCYl1Zc= alvar@LAPTOP-JU9DOLHH"
   }
   os_disk {
