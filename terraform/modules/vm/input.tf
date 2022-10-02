b543d421cc86842be6e2b8e6569159dd726fe83b
Álvaro Gómez <alvarogomen93@gmail.com>
2022-10-02 17:43:50 +0200

    Update input.tf

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -1,1 +1,7 @@
-
+variable "location" {}
+variable "application_type" {}
+variable "resource_type" {}
+variable "resource_group" {}
+variable "vm_admin_username" {}
+variable "subnet_id" {}
+variable "public_ip_address_id" {}
